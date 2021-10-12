258c2922c16ec5dbaea1cd9544e5d486b68fb574
Daniel Méndez Yáñez <dmendezy87@gmail.com>
2021-10-12 10:31:49 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // Opcional
+import { appReducers } from './app.reducer';
 
 import { AppComponent } from './app.component';
+import { TodosModule } from './todos/todos.module';
+import { FooterComponent } from './footer/footer.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot( appReducers ),
+    // Opcional
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+    TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
